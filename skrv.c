4d73d736b654aabc7002613a8cb2c05b2bf6c719
Joel Bretheim <jbretheim@gmail.com>
2017-04-18 14:41:29 -0400

    clears screen at exit

--- skrv.c
+++ skrv.c
@@ -21,6 +21,9 @@ struct termios orig_termios;
 
 /* error handling */
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);   /* from stdio  */
   exit(1);     /* from stdlib */
 }
@@ -67,6 +70,8 @@ void editorProcessKeypress() {
   char c = editorReadKey();
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
